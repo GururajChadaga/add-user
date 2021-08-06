7fc67102a4b1f3536f211c84a7554c98052c530e
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-08-06 20:04:07 +0530

    Used portals to render modal overlays next to Document root

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,9 +2,13 @@ import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 import { Fragment } from 'react';
-const ErrorModal = (props) => {
-  return <Fragment>
-    <div className={classes.backdrop} onClick={props.onConfirm}></div>
+import ReactDom from 'react-dom';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+};
+const ModalOverlay = (props) => {
+  return (
     <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
@@ -16,7 +20,26 @@ const ErrorModal = (props) => {
         <Button onClick={props.onConfirm}>Okay</Button>
       </footer>
     </Card>
-  </Fragment>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </Fragment>
+  );
 };
 
 export default ErrorModal;
